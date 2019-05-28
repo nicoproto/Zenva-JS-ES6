b3bf104c321ec3ef60bb95eb6dcfa954cc413e0d
Nicolás Proto <nicolasproto@gmail.com>
2019-05-28 17:07:01 -0300

    Added server class

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-const request = require('request');
-const http = require('http');
-const cryptocoin = require('./cryptocoin.js');
+const server = require('./Server.js');
 
-let coins = [];
-
-http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  if (coins) {
-    res.end(JSON.stringify(coins));
-  } else {
-    res.end('No data');
-  };
-}).listen(8080);
-
-request('https://api.coinmarketcap.com/v1/ticker/', (err, request_res, body) => {
-  if (err) throw err;
-
-  let coin_data = JSON.parse(body);
-  coin_data.forEach((coin) => coins.push(new cryptocoin.Cryptocoin(coin.id, coin.name, coin.price_usd)));
-
-  coin_data = body;
-});
+const server_object = new server.Server(8080);
